7f33927488a3f98f47488e1cbf05ffdfcf5c25f3
thiago.pelikan <thiago.pelikan@gmail.com>
2025-09-21 12:44:21 -0300

    fix port

--- mcp_server.py
+++ mcp_server.py
@@ -83,4 +83,6 @@ def listar_tools_mcp():
     return jsonify({"tools": MCP_TOOLS})
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
